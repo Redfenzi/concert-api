254d7f624a015b7c3758e6056c63f07cddff3b61
Redfenzi <redouanefenzi@gmail.com>
2023-02-09 00:54:48 +0100

    feat: Création de l'interface SalleService et implémentation des services

--- src/main/java/com/services/SalleService.java
+++ src/main/java/com/services/SalleService.java
@@ -1,6 +1,15 @@
 package com.services;
 
+import com.dtos.DogDto;
+import com.dtos.SalleDto;
+
+import java.util.List;
+
 public interface SalleService {
 
+    SalleDto saveSalle(SalleDto salleDto);
+    SalleDto getSalleById(Long salleId);
+    boolean deleteSalle(Long salleId);
+    List<SalleDto> getAllSalles();
 
 }
